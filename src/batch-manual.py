99caaad2cf6c69794988ebe165fd5b5c13dd46d7
유재성 <kade@afidev.com>
2023-07-25 13:13:02 +0900

    [add] concat_data

--- src/batch-manual.py
+++ src/batch-manual.py
@@ -2,37 +2,48 @@ import os
 import sys
 import time
 
+import polars as pl
+
 from utils import get_from_to
 from stocks import get_aggregates, aggs_to_df
 
 
-def save_data(ticker, year, month):
+def save_data(ticker, year, month, skip_if_exists=True):
     from_, to = get_from_to(year, month)
+
+    if skip_if_exists:
+        if os.path.exists(f"data/{year}_{month:02d}.parquet"):
+            print(f"Skipping {year}_{month:02d}.parquet")
+            return
 
     aggs = get_aggregates(ticker=ticker, from_=from_, to=to)
 
     df = aggs_to_df(aggs, gte=from_, lt=to)
 
-    print(df)
+    print(df)  # limit 5 requests in 1 minute
 
     os.makedirs("data", exist_ok=True)
     df.write_parquet(f"data/{year}_{month:02d}.parquet")
+
+    time.sleep(12)
     return
+
+
+def concat_data(ticker):
+    df = pl.read_parquet("data/*.parquet")
+    print(df)
+
+    df.write_parquet(f"data/{ticker}.parquet")
 
 
 if __name__ == "__main__":
     ticker = "SPY"
-    if sys.argv[1] == "all":
-        year = 2022
-        for m in range(1, 13):
-            save_data(ticker, year, month=m)
-            time.sleep(12)  # limit 5 requests in 1 minute
+    year = 2022
+    for m in range(1, 13):
+        save_data(ticker, year, month=m)
 
-        year = 2023
-        for m in range(1, 8):
-            save_data(ticker, year, month=m)
+    year = 2023
+    for m in range(1, 8):
+        save_data(ticker, year, month=m)
 
-    else:
-        year, month = int(sys.argv[1]), int(sys.argv[2])
-
-        save_data(ticker, year, month)
+    concat_data(ticker)
